74fa7a72a317048754458a9cafc24ac9449df64d
安圣Shy_Go <1521192460@qq.com>
2026-09-11 17:01:02 +0800

    新增文件：关于用户页面和管理员页面的切换

---
+++ 11.STM32F401RET6-管理员页面和用户页面切换/USER/inc/nvic.h
@@ -0,0 +1,20 @@
+#ifndef _NVIC_H
+#define _NVIC_H
+#include "main.h"
+
+//串口1接收数据结构体定义
+typedef struct
+{
+    u8  buff[10];    //接收数据缓冲区
+    u16 len;         //记录接收数据的长度
+    u8  flag;        //接收数据完成标志位
+}USART_STATUE;
+
+extern USART_STATUE u1;
+extern USART_STATUE u6;
+
+//宏定义
+
+//函数声明
+#endif
+
---
+++ 11.STM32F401RET6-管理员页面和用户页面切换/USER/inc/timer.h
@@ -0,0 +1,12 @@
+#ifndef _TIMER_H
+#define _TIMER_H
+#include "main.h"
+
+//宏定义
+
+
+//函数声明
+void timer9_delay_ms(u16 ms);
+
+
+#endif
